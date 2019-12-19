8adcb76053ff9d131c1d43c5bf1b115ac71e462a
HaikunHuang <quincy.huanghk@gmail.com>
2019-12-18 22:25:14 -0500

    commit to master

--- hf.js
+++
@@ -1,6 +0,0 @@
-// JavaScript Document
-jQuery(document).ready(function($){
-    "use strict";
-    $("<div/>").load("header.html").insertBefore($(".page-content"));
-    $("<div/>").load("footer.html").insertAfter($(".page-content"));
-});
